0b875b3e5f69a8debc4faf99b8bb2ccfa69d3ccb
DD <50895419+DD513@users.noreply.github.com>
2022-03-29 10:04:46 +0800

    fix Profile

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ import morgan from "morgan";
 import passport from "passport";
 import session from "express-session";
 import index from "./routes/index"; // 用來 存放route的接口
-
-require("dotenv").config();
+import "dotenv/config";
+// require("dotenv").config();
 
 const app = express();
 
@@ -33,7 +33,7 @@ app.use(passport.session());
 app.use("/api", index); // 後面要建立到routus/index.js才能使用
 
 const server = http.createServer(app);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log("App is running on port " + port);
